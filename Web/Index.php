a12b4f212adfac4b11dd0244460491a0ae3a23fe
l2griffon <59934869+l2griffon@users.noreply.github.com>
2020-01-16 11:17:52 -0300

    Update and rename web/index.php to Web/Index.php

--- Web/Index.php
+++ Web/Index.php
@@ -7,16 +7,16 @@ $app['debug'] = true;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-  array(
-   'pdo.server' => array(
-   'driver'   => 'pgsql',
-   'user' => $dbopts["user"],
-   'password' => $dbopts["pass"],
-   'host' => $dbopts["host"],
-   'port' => $dbopts["port"],
-   'dbname' => ltrim($dbopts["path"],'/')
-  )
- )
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
 );
 
 // Register the monolog logging service
